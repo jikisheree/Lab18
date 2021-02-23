b1fc5f7ff8c4950581a1fb001f7e52d002c86c6c
username <email@gmail.com>
2021-02-23 16:41:50 +0700

    finish

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,19 @@
 #include<iostream>
 using namespace std;
 
-struct Rect{55
+struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+	double overlap(Rect r1,Rect r2){
+	double y;
+	double x;
 
+ 	y = min( r1.y , r2.y ) - max( r1.y-r1.h , r2.y-r2.h );
+ 	x = min( r1.x + r1.w , r2.x+r2.w ) - max( r1.x , r2.x);
+	if(x*y<=0){return 0;}else{
+	return x*y;
+}
 
 
 }
